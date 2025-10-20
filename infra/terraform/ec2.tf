ce4861f338e633bd72ba472b5bda450792fece1f
scriptJohnmns <johnmnavess@gmail.com>
2025-10-20 13:45:16 -0300

    fix: ec2.tf

--- infra/terraform/ec2.tf
+++ infra/terraform/ec2.tf
@@ -24,7 +24,7 @@ resource "aws_security_group" "pyhostname_sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   security_group_id = aws_security_group.pyhostname_sg.id
-  cidr_ipv4         = "0.0.0.0/0" #<-- aqui ficava meu ip...
+  cidr_ipv4         = "0.0.0.0/0" 
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
